0d2fac22904f53b34a74ec03bad615b64dc2db82
CaroMV <carolina.moreno.v@ug.uchile.cl>
2023-08-14 19:07:13 -0400

    CM: Funciona el mensaje de número resultados incluso cuando hay 0

--- src/main.js
+++ src/main.js
@@ -65,142 +65,147 @@ audio.volume = 0.5;
 
 // * Función mostrar tarjetas
 const mostrarPeliculas = (peliculas) => {
-  // contenedorDelContenedorDeTarjetas.innerHTML = "";
-  peliculas.forEach(pelicula => {
-  // Mostrar la cantidad de películas encontradas
-    peliculasEncontradas.textContent = peliculas.length + " movies found"
+  if (peliculas.length === 0){
+    peliculasEncontradas.textContent = "0 movies found";
+    contenedorDelContenedorDeTarjetas.innerHTML = ""
+  } else {
+    contenedorDelContenedorDeTarjetas.innerHTML = "";
+    peliculas.forEach(pelicula => {
+      // Mostrar la cantidad de películas encontradas
+      peliculasEncontradas.textContent = peliculas.length + " movies found"
 
-    // Crear el Div contenedor donde estará la tarjeta
-    const contenedorTarjeta = document.createElement("article");
-    contenedorTarjeta.classList.add("contenedor-tarjeta");
+      // Crear el Div contenedor donde estará la tarjeta
+      const contenedorTarjeta = document.createElement("article");
+      contenedorTarjeta.classList.add("contenedor-tarjeta");
   
-    // Article de la tarjeta
-    const tarjetaPelicula = document.createElement("article");
-    tarjetaPelicula.classList.add("tarjeta-pelicula");
+      // Article de la tarjeta
+      const tarjetaPelicula = document.createElement("article");
+      tarjetaPelicula.classList.add("tarjeta-pelicula");
   
-    // Div contenedor imagen
-    const contenedorImagen = document.createElement("div");
-    contenedorImagen.classList.add("contenedor-img");
-    // Añadiendo el Div contenedor imagen a la tarjeta
-    tarjetaPelicula.appendChild(contenedorImagen)
+      // Div contenedor imagen
+      const contenedorImagen = document.createElement("div");
+      contenedorImagen.classList.add("contenedor-img");
+      // Añadiendo el Div contenedor imagen a la tarjeta
+      tarjetaPelicula.appendChild(contenedorImagen)
   
-    // Imagen portada
-    const imagen = document.createElement("img");
-    imagen.src = pelicula.poster;
-    imagen.alt = "portada-película";
-    imagen.id = "portada";
-    contenedorImagen.appendChild(imagen);
+      // Imagen portada
+      const imagen = document.createElement("img");
+      imagen.src = pelicula.poster;
+      imagen.alt = "portada-película";
+      imagen.id = "portada";
+      contenedorImagen.appendChild(imagen);
   
-    // ! Div contenedor información
-    const contenedorInfo = document.createElement("div");
-    contenedorInfo.classList.add("contenedor-info");
-    tarjetaPelicula.appendChild(contenedorInfo);
+      // ! Div contenedor información
+      const contenedorInfo = document.createElement("div");
+      contenedorInfo.classList.add("contenedor-info");
+      tarjetaPelicula.appendChild(contenedorInfo);
   
-    // * Elementos dentro del contenedorInfo
-    // Título película
-    const titulo = document.createElement("h3");
-    titulo.id = "titulo";
-    titulo.textContent = pelicula.title;
-    contenedorInfo.appendChild(titulo);
-    // Descripción span y p
-    const descripcionSpan = document.createElement("span");
-    descripcionSpan.id = "descripcion";
-    descripcionSpan.textContent = "Description";
-    contenedorInfo.appendChild(descripcionSpan);
+      // * Elementos dentro del contenedorInfo
+      // Título película
+      const titulo = document.createElement("h3");
+      titulo.id = "titulo";
+      titulo.textContent = pelicula.title;
+      contenedorInfo.appendChild(titulo);
+      // Descripción span y p
+      const descripcionSpan = document.createElement("span");
+      descripcionSpan.id = "descripcion";
+      descripcionSpan.textContent = "Description";
+      contenedorInfo.appendChild(descripcionSpan);
   
-    const descripcion = document.createElement("p");
-    descripcion.id = "descripcion";
-    descripcion.textContent = pelicula.description;
-    contenedorInfo.appendChild(descripcion);
+      const descripcion = document.createElement("p");
+      descripcion.id = "descripcion";
+      descripcion.textContent = pelicula.description;
+      contenedorInfo.appendChild(descripcion);
   
-    // ! Div director, productor, fecha y score
-    const infoDiv = document.createElement("div");
-    infoDiv.classList.add("director-productor-fecha-puntuacion");
+      // ! Div director, productor, fecha y score
+      const infoDiv = document.createElement("div");
+      infoDiv.classList.add("director-productor-fecha-puntuacion");
   
-    // * Elementos dentro del div director, productor, fecha y score
-    // Div director
-    const directorDiv = document.createElement("div");
-    directorDiv.classList.add("info");
-    const directorSpan = document.createElement("span");
-    directorSpan.textContent = "Director:";
-    const directorP = document.createElement("p");
-    directorP.id = "director";
-    directorP.textContent = pelicula.director;
-    directorDiv.appendChild(directorSpan);
-    directorDiv.appendChild(directorP);
-    infoDiv.appendChild(directorDiv);
+      // * Elementos dentro del div director, productor, fecha y score
+      // Div director
+      const directorDiv = document.createElement("div");
+      directorDiv.classList.add("info");
+      const directorSpan = document.createElement("span");
+      directorSpan.textContent = "Director:";
+      const directorP = document.createElement("p");
+      directorP.id = "director";
+      directorP.textContent = pelicula.director;
+      directorDiv.appendChild(directorSpan);
+      directorDiv.appendChild(directorP);
+      infoDiv.appendChild(directorDiv);
   
-    // Div Productor
-    const producerDiv = document.createElement("div");
-    producerDiv.classList.add("info");
-    const producerSpan = document.createElement("span");
-    producerSpan.textContent = "Producer:";
-    const producerP = document.createElement("p");
-    producerP.id = "productor";
-    producerP.textContent = pelicula.producer;
-    producerDiv.appendChild(producerSpan);
-    producerDiv.appendChild(producerP);
-    infoDiv.appendChild(producerDiv);
+      // Div Productor
+      const producerDiv = document.createElement("div");
+      producerDiv.classList.add("info");
+      const producerSpan = document.createElement("span");
+      producerSpan.textContent = "Producer:";
+      const producerP = document.createElement("p");
+      producerP.id = "productor";
+      producerP.textContent = pelicula.producer;
+      producerDiv.appendChild(producerSpan);
+      producerDiv.appendChild(producerP);
+      infoDiv.appendChild(producerDiv);
   
-    // Div Fecha
-    const dateDiv = document.createElement("div");
-    dateDiv.classList.add("info");
-    const dateSpan = document.createElement("span");
-    dateSpan.textContent = "Release Date:";
-    const dateP = document.createElement("p");
-    dateP.id = "fecha";
-    dateP.textContent = pelicula.release_date;
-    dateDiv.appendChild(dateSpan);
-    dateDiv.appendChild(dateP);
-    infoDiv.appendChild(dateDiv);
+      // Div Fecha
+      const dateDiv = document.createElement("div");
+      dateDiv.classList.add("info");
+      const dateSpan = document.createElement("span");
+      dateSpan.textContent = "Release Date:";
+      const dateP = document.createElement("p");
+      dateP.id = "fecha";
+      dateP.textContent = pelicula.release_date;
+      dateDiv.appendChild(dateSpan);
+      dateDiv.appendChild(dateP);
+      infoDiv.appendChild(dateDiv);
   
-    //Div Score
-    const scoreDiv = document.createElement("div");
-    scoreDiv.classList.add("info");
-    const scoreSpan = document.createElement("span");
-    scoreSpan.textContent = "Score:";
-    const scoreP = document.createElement("p");
-    scoreP.id = "puntuacion";
-    scoreP.textContent = pelicula.rt_score;
-    scoreDiv.appendChild(scoreSpan);
-    scoreDiv.appendChild(scoreP);
-    infoDiv.appendChild(scoreDiv);
+      //Div Score
+      const scoreDiv = document.createElement("div");
+      scoreDiv.classList.add("info");
+      const scoreSpan = document.createElement("span");
+      scoreSpan.textContent = "Score:";
+      const scoreP = document.createElement("p");
+      scoreP.id = "puntuacion";
+      scoreP.textContent = pelicula.rt_score;
+      scoreDiv.appendChild(scoreSpan);
+      scoreDiv.appendChild(scoreP);
+      infoDiv.appendChild(scoreDiv);
   
-    contenedorInfo.appendChild(infoDiv);
+      contenedorInfo.appendChild(infoDiv);
   
-    //Div botones
-    const botonContenedor = document.createElement("div");
-    botonContenedor.classList.add("contenedor-btn");
+      //Div botones
+      const botonContenedor = document.createElement("div");
+      botonContenedor.classList.add("contenedor-btn");
   
-    // Boton personajes
-    const botonPersonajes = document.createElement("button");
-    botonPersonajes.classList.add("btn");
-    botonPersonajes.classList.add("btn-personajes");
-    botonPersonajes.textContent = "Characters";
-    botonPersonajes.id="btn-personajes";
-    botonPersonajes.setAttribute("data-id", pelicula.id) // para tener contexto y poder generar los personajes
-    botonContenedor.appendChild(botonPersonajes);
+      // Boton personajes
+      const botonPersonajes = document.createElement("button");
+      botonPersonajes.classList.add("btn");
+      botonPersonajes.classList.add("btn-personajes");
+      botonPersonajes.textContent = "Characters";
+      botonPersonajes.id="btn-personajes";
+      botonPersonajes.setAttribute("data-id", pelicula.id) // para tener contexto y poder generar los personajes
+      botonContenedor.appendChild(botonPersonajes);
   
-    // Boton Lugares
-    const botonLugares = document.createElement("button");
-    botonLugares.classList.add("btn");
-    botonLugares.textContent = "Locations";
-    botonLugares.id="btn-lugares";
-    botonLugares.setAttribute("data-id", pelicula.id) // para tener contexto y poder generar los personajes
-    botonContenedor.appendChild(botonLugares);
+      // Boton Lugares
+      const botonLugares = document.createElement("button");
+      botonLugares.classList.add("btn");
+      botonLugares.textContent = "Locations";
+      botonLugares.id="btn-lugares";
+      botonLugares.setAttribute("data-id", pelicula.id) // para tener contexto y poder generar los personajes
+      botonContenedor.appendChild(botonLugares);
   
-    contenedorInfo.appendChild(botonContenedor);
+      contenedorInfo.appendChild(botonContenedor);
   
-    // Agregar contenedorinfo dentro de tarjetaPelicula
-    tarjetaPelicula.appendChild(contenedorInfo);
+      // Agregar contenedorinfo dentro de tarjetaPelicula
+      tarjetaPelicula.appendChild(contenedorInfo);
   
-    // Agregar tarjetaPelicula dentro de contenedorTarjetas
-    contenedorTarjeta.appendChild(tarjetaPelicula);
+      // Agregar tarjetaPelicula dentro de contenedorTarjetas
+      contenedorTarjeta.appendChild(tarjetaPelicula);
   
-    // Agregar contenedorTarjetas dentro de seccionTarjetas
-    contenedorDelContenedorDeTarjetas.appendChild(contenedorTarjeta);
+      // Agregar contenedorTarjetas dentro de seccionTarjetas
+      contenedorDelContenedorDeTarjetas.appendChild(contenedorTarjeta);
 
-  });
+    });
+  }
   obtenerBotonesPersonajes()
 }
 function obtenerBotonesPersonajes (){
@@ -246,7 +251,8 @@ ordenarPorAlfabeto.addEventListener("change", () => {
 // * Función para buscar películas
 btnBuscar.addEventListener("click", () => {
   const peliculasEncontradas = buscarTermino(inputBuscar.value);
-  mostrarPeliculas(peliculasEncontradas);
+  resultados=[...peliculasEncontradas];
+  mostrarPeliculas(resultados);
   // Vaciar el input para buscar nuevamente
   inputBuscar.value = "";
   window.scrollTo(0, 1000);
